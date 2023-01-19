09435b04f7397440070272731cebea5a164cb854
Oliver Rasoli <122869683+oliverRasoli@users.noreply.github.com>
2023-01-19 10:13:02 +0100

    fix: i18next implementation

--- src/ui/pages/tabs/tab-1/Tab1.tsx
+++ src/ui/pages/tabs/tab-1/Tab1.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { IonContent, IonTitle } from '@ionic/react';
 import TakePicture from 'ui/components/frontpage/take-picture/TakePicture';
 import { Photo } from '@capacitor/camera';
+import { t } from 'i18next';
 
 const Tab1: React.FC = () => {
   const [picture, setPicture] = React.useState<Photo>();
 
   return (
     <IonContent color={'white-background'}>
-      {!picture && <IonTitle>Take a photo!</IonTitle>}
+      {!picture && <IonTitle>{t('tabContent.takeAPhoto')}</IonTitle>}
       {picture && <img src={picture?.webPath} alt="your upload" className="h-full w-auto m-auto" />}
 
       <TakePicture onPictureTaken={(p) => setPicture(p)} />
