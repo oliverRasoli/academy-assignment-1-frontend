20adb5bde903466d6ffd65902b70cfadad95ce59
Oliver Rasoli <122869683+oliverRasoli@users.noreply.github.com>
2023-01-19 14:43:23 +0100

    fix: i18 implementation

--- src/ui/pages/tabs/tab-2/Tab2.tsx
+++ src/ui/pages/tabs/tab-2/Tab2.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { IonContent, IonTitle } from '@ionic/react';
+import { t } from 'i18next';
 
 const Tab2: React.FC = () => (
   <IonContent color={'white-background'}>
-    <IonTitle>Tab2</IonTitle>
+    <IonTitle>{t('profilePage.title')}</IonTitle>
   </IonContent>
 );
 
